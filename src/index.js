85963762f55f63bfae515e0d37c0a90ab7b908d8
brilliahib <brilliahib21@gmail.com>
2024-12-07 14:14:43 +0700

    fix: fixing when file is large

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const predictRoute = require("./routes/predictRoute");
 const historyRoute = require("./routes/historyRoute");
+const { errorMiddleware } = require("./middlewares/errorMiddleware");
 
 const app = express();
 app.use(bodyParser.json());
 app.use("/predict", predictRoute);
 app.use("/predict/histories", historyRoute);
 
-const PORT = process.env.PORT || 8080;
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
